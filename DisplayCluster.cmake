e1357319612c6b8bafc0883eac18288e986521ac
Madhu Srinivasan <madhu.srinivasan@kaust.edu.sa>
2014-12-10 11:12:22 +0300

    Giving up on install paths

--- DisplayCluster.cmake
+++ DisplayCluster.cmake
@@ -1,3 +1,4 @@
 set(DISPLAYCLUSTER_REPO_URL https://github.com/kaust-vislab/DisplayCluster.git)
-set(DISPLAYCLUSTER_CMAKE_ARGS -DCMAKE_BUILD_TYPE=Debug)
-set(DISPLAYCLUSTER_CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=/home/srinivm/software/DisplayCluster/master)
+set(DISPLAYCLUSTER_CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release)
+#MADHU: Buildyard ignores this argument for some reason???
+#set(DISPLAYCLUSTER_CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=/home/srinivm/software/DisplayCluster/master)
